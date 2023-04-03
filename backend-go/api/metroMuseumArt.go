7adeb0a3d6e14eda2fbc9d5c535cfab5166e29dd
Michael Chan <michaelcychan@gmail.com>
2023-04-03 19:28:18 +0100

    refactor MMA api to a separate file

---
+++ backend-go/api/metroMuseumArt.go
@@ -0,0 +1,53 @@
+// Metropolitan Museum of Art
+// https://metmuseum.github.io/#search
+
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type MMAArtworkSimple struct {
+	Title     string `json:"title"`
+	ID        int    `json:"objectID"`
+	Artist    string `json:"artistDisplayName"` // there is artistDisplayAlphaSort, check that if necessary
+	ImageURL  string `json:"primaryImageSmall"`
+	DateStart int    `json:"objectBeginDate"`
+	DateEnd   int    `json:"objectEndDate"`
+}
+
+func GetArtworkMetropolitanMusuemArt(artworkID int, c *fiber.Ctx) (MMAArtworkSimple, error) {
+	const MetMuseumOfArtBaseUri = "https://collectionapi.metmuseum.org/public/collection/v1"
+
+	var artwork MMAArtworkSimple
+
+	a := fiber.AcquireAgent()
+	req := a.Request()
+	req.Header.SetMethod(fiber.MethodGet)
+	req.SetRequestURI(MetMuseumOfArtBaseUri + "/objects" + "/" + fmt.Sprint(artworkID))
+
+	if err := a.Parse(); err != nil {
+		return artwork, fmt.Errorf("error retrieving info from external api: %s", err)
+	}
+
+	code, bodyBytes, errs := a.Bytes()
+
+	if len(errs) != 0 {
+		return artwork, fmt.Errorf("errors returned from external api: %s", errs)
+	}
+
+	if code != http.StatusOK {
+		return artwork, fmt.Errorf("return code from api not OK: %d", code)
+	}
+
+	if unmarshalErr := json.Unmarshal(bodyBytes, &artwork); unmarshalErr != nil {
+		return artwork, fmt.Errorf("json unmarshal error")
+	}
+
+	return artwork, nil
+
+}
--- backend-go/handler/metMAHandler.go
+++ backend-go/handler/metMAHandler.go
@@ -4,56 +4,20 @@
 package handler
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"math/rand"
-	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/michaelcychan/art-notebook/backend-go/api"
 )
 
-type MMAArtworkSimple struct {
-	Title     string `json:"title"`
-	ID        int    `json:"objectID"`
-	Artist    string `json:"artistDisplayName"` // there is artistDisplayAlphaSort, check that if necessary
-	ImageURL  string `json:"primaryImageSmall"`
-	DateStart int    `json:"objectBeginDate"`
-	DateEnd   int    `json:"objectEndDate"`
-}
-
 func GetExampleMetMAArt(c *fiber.Ctx) error {
-	const MetMuseumOfArtBaseUri = "https://collectionapi.metmuseum.org/public/collection/v1"
-
 	listOfExmapleArtid := []int{437133}
 	exampleArtid := listOfExmapleArtid[rand.Intn(len(listOfExmapleArtid))]
 
-	a := fiber.AcquireAgent()
-	req := a.Request()
-	req.Header.SetMethod(fiber.MethodGet)
-	req.SetRequestURI(MetMuseumOfArtBaseUri + "/objects" + "/" + fmt.Sprint(exampleArtid))
+	artwork, err := api.GetArtworkMetropolitanMusuemArt(exampleArtid, c)
 
-	if err := a.Parse(); err != nil {
-		log.Fatalf("error retrieving info from external api")
-		return c.Status(500).JSON(fiber.Map{"message": "external api error"})
-	}
-
-	code, bodyBytes, errs := a.Bytes()
-
-	if len(errs) != 0 {
-		log.Fatalln("errors returned from external api")
-		return c.Status(500).JSON(fiber.Map{"message": "external api error"})
-	}
-
-	if code != http.StatusOK {
-		log.Fatalf("return code from api not OK: %d", code)
-		return c.Status(500).JSON(fiber.Map{"message": "external api error"})
-	}
-
-	var artwork MMAArtworkSimple
-
-	if unmarshalErr := json.Unmarshal(bodyBytes, &artwork); unmarshalErr != nil {
-		return c.Status(500).JSON(fiber.Map{"message": "error"})
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"message": "server error"})
 	}
 
 	artistSlice := []string{artwork.Artist}
